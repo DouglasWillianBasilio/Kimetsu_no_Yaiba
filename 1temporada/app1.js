9f5cba24866ee1508c1cc253d3b1d6e09633a4a9
Douglas1301 <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-03-06 18:27:53 -0300

    arrumando menus

--- 1temporada/app1.js
+++ 1temporada/app1.js
@@ -1,9 +1,19 @@
 function abrirmenu() {
     let BarraMenuAberto = document.getElementById ('barra-menu')
-    
+
     if(BarraMenuAberto.style.width == '0px') {
-        BarraMenuAberto.style.width = '300px'
+        BarraMenuAberto.style.width = '450px'
     } else{
         BarraMenuAberto.style.width = '0px'
     }
-}
+    if(btns.style.display == 'block' ){
+        btns.style.display = 'none' 
+    } else{
+            btns.style.display = 'block' 
+        }
+    }
+    
+
+
+
+
